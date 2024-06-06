fe0da2fe9060e9116269b74870c80387d4f8a41f
snowman <hxflove521@126.com>
2024-06-06 13:42:15 +0800

    Add File

---
+++ 8_func.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+#
+
+#函数定义
+function hello(){
+    echo "Hey! Bro!, your Input $1 $2 $3,一共有$#个参数"
+}
+
---
+++ 8_func_main.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+#
+[ -f /home/he/study_shell/shell/8_func.sh ] && . /home/he/study_shell/shell/8_func.sh || exit
+
+hello $1 $2 $3
